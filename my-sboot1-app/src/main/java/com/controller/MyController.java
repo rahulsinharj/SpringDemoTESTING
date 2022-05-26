b86f4646f268caa5cfc331b1f6afceb56b3141c9
rahulsinharj <rahulkumarsinha1234@gmail.com>
2022-05-27 02:07:09 +0530

    added controller

--- my-sboot1-app/src/main/java/com/controller/MyController.java
+++ my-sboot1-app/src/main/java/com/controller/MyController.java
@@ -1,12 +1,19 @@
 package com.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
+    @RequestMapping("/do")
     public String doSomething()
     {
-        return "rahulsinha is my name";
+        return "RahulkSinha is my name ok Sinha ji !";
+    }
+
+    public String mustDo()
+    {
+        return "RahulSinha you must do these things";
     }
 }
