a78053a8a0d5258451d097e7439f5c8dcd731f00
rahulsinharj <rahulkumarsinha1234@gmail.com>
2022-05-27 01:28:54 +0530

    added controller

---
+++ my-sboot1-app/src/main/java/com/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    public String doSomething()
+    {
+        return "rahulsinha is my name";
+    }
+}
